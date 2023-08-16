c7d75a79789902d2c157a8ee3ed2b876a707ee05
Nick Fistere <nwfistere@gmail.com>
2023-08-16 15:03:40 -0500

    It works

--- src/VSMenuHelper.cs
+++ src/VSMenuHelper.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
+using static Il2CppTMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 using static Il2CppVampireSurvivors.UI.OptionsController;
 
 namespace VSMenuModHelper
@@ -99,56 +99,22 @@ namespace VSMenuModHelper
                 return false;
             }
         }
+
+        public override void OnLateUpdate()
+        {
+            base.OnLateUpdate();
+        }
 #endif
 
         [HarmonyPatch(typeof(OptionsController))]
         class OptionsController_Patch2
         {
 
-            static RectTransform _content;
+            public static RectTransform _content;
+            public static GameObject tabObject;
 
-            //[HarmonyPatch(nameof(OptionsController.Initialize))]
-            //[HarmonyPrefix]
-            //static void Initialize_Prefix2(OptionsController __instance)
-            //{
-            //    //Texture2D[] textures = Resources.LoadAll<Texture2D>("");
-            //    //List<string> names = textures.Select(t => t.name).ToList();
-            //    //Component[] prefabs = Resources.LoadAll<Component>("");
-            //    //List<string> prefabsnames = prefabs.Select(t => t.name).ToList();
-            //    //Sprite[] uiSprites = Resources.LoadAll<Sprite>("spritesheets/UI");
-
-            //    GameObject TabScroll = new GameObject("TabScroll", Il2CppType.Of<RectTransform>());
-            //    TabScroll.transform.SetParent(__instance._TabContainer, false);
-
-            //    List<string> gameObjects = Resources.LoadAll<GameObject>("").Select((g) => g.name).ToList();
-
-            //    //prefabs = Resources.LoadAll<Component>("PrefabInstance");
-            //    //prefabsnames = prefabs.Select(t => t.name).ToList();
-
-            //    GameObject viewport = new GameObject("Viewport", Il2CppType.Of<RectTransform>());
-            //    viewport.transform.SetParent(TabScroll.transform, false);
-
-
-            //    GameObject content = new GameObject("Content", Il2CppType.Of<RectTransform>());
-            //    content.transform.SetParent(viewport.transform, false);
-
-            //    //UnityEngine.Object.Instantiate()
-            //    GameObject Scrollbar = new GameObject("Scrollbar Vertical", Il2CppType.Of<Scrollbar>());
-            //    Scrollbar.transform.SetParent(TabScroll.transform);
-
-            //    GameObject Slider = new GameObject("Slider", Il2CppType.Of<Scrollbar>());
-
-            //    ScrollEnhancer enhancer = TabScroll.AddComponent<ScrollEnhancer>();
-            //    //Scrollbar scrollbar = enhancer.gameObject.AddComponent<Scrollbar>();
-            //    UnityEngine.UI.Slider slider = enhancer.gameObject.AddComponent<UnityEngine.UI.Slider>();
-            //    enhancer.Initialize(1, viewport.GetComponent<RectTransform>(), Scrollbar.GetComponent<Scrollbar>(), slider, 0.1f);
-
-            //    _content = content;
-            //}
-
-            static RectTransform CreateScrollViewContent(ScrollRect scrollRect)
+            static RectTransform CreateScrollViewContent(RectTransform viewportRectTransform)
             {
-                // Create a Content GameObject and set its properties.
                 GameObject contentObject = new GameObject("Content");
                 contentObject.AddComponent<RectTransform>(); // Required for layout control.
                 contentObject.AddComponent<VerticalLayoutGroup>();
@@ -158,78 +124,74 @@ namespace VSMenuModHelper
                 layoutGroup.childControlHeight = false;
                 layoutGroup.childControlWidth = false;
                 layoutGroup.spacing = 55;
-                //layoutGroup.padding = new RectOffset(0, 0, 0, 40);
 
-                // Set the Content's RectTransform properties.
                 RectTransform contentRectTransform = contentObject.GetComponent<RectTransform>();
-                contentRectTransform.SetParent(scrollRect.transform, false);
-                //contentRectTransform.SetParent(contentObject.transform, false);
-                contentRectTransform.anchorMin = Vector2.up;
+                contentRectTransform.SetParent(viewportRectTransform, false);
+                contentRectTransform.anchorMin = Vector2.zero;
                 contentRectTransform.anchorMax = Vector2.one;
                 contentRectTransform.pivot = Vector2.up;
+                contentRectTransform.anchoredPosition = new Vector2(30, 0);
 
                 ContentSizeFitter contentSizeFitter = contentObject.GetComponent<ContentSizeFitter>();
                 contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
-
-                // Add your UI elements (Text, Images, etc.) to the contentGameObject.
-
-                layoutGroup.SetDirty();
 
                 return contentRectTransform;
             }
 
             static RectTransform CreateScrollViewViewport(GameObject scrollViewObject)
             {
-                // Create a Viewport GameObject and set its properties.
+                Material material = new Material(Shader.Find("UI/Default"));
+                Material unmaterial = new Material(Shader.Find("UI/Default"));
                 GameObject viewportObject = new GameObject("Viewport");
                 viewportObject.AddComponent<RectTransform>(); // Required for layout control.
 
-                // Set the Viewport's RectTransform properties.
                 RectTransform viewportRectTransform = viewportObject.GetComponent<RectTransform>();
-                viewportRectTransform.SetParent(scrollViewObject.transform, false);
+                viewportRectTransform.SetParent(scrollViewObject.GetComponent<RectTransform>(), false);
 
-                viewportRectTransform.anchorMin = Vector2.zero;
+                viewportRectTransform.anchorMin = new Vector2(0, 0.02f);
                 viewportRectTransform.anchorMax = Vector2.one;
                 viewportRectTransform.sizeDelta = Vector2.zero;
+                viewportRectTransform.anchoredPosition = new Vector2(0, -10);
 
-                Mask displayMask = GameObject.Find("UI/Canvas - App/Safe Area/View - Options/OptionsController/Display/Scroll/Viewport").GetComponent<Mask>();
+                Sprite[] allSprites = Resources.FindObjectsOfTypeAll<Sprite>();
 
-                // Create a Mask component and add it to the Viewport GameObject.
+                Sprite sprite = allSprites.Where(s => s.name == "UIMask").FirstOrDefault(null as Sprite);
+
                 Mask mask = viewportObject.AddComponent<Mask>();
-                mask.showMaskGraphic = true; // Set to true if you want to see the masked area.
-                mask.m_RectTransform = viewportRectTransform;
-                mask.m_Graphic = displayMask.m_Graphic;
-                mask.m_MaskMaterial = displayMask.m_MaskMaterial;
-                mask.m_UnmaskMaterial = displayMask.m_UnmaskMaterial;
+                mask.showMaskGraphic = false;
 
-                mask = displayMask;
+                Image image = viewportObject.AddComponent<Image>();
+                image.m_Type = Image.Type.Sliced;
+                image.material = material;
+                //image.isMaskingGraphic = true;
+                image.sprite = sprite;
 
-                UnityEngine.UI.Image maskImage = viewportObject.AddComponent<UnityEngine.UI.Image>();
-                maskImage.sprite = SpriteImporter.LoadSprite(mask.m_Graphic.mainTexture.Cast<Texture2D>());
-                //maskImage.mainTexture = mask.m_Graphic.mainTexture;
+                mask.m_Graphic = image;
+                mask.m_MaskMaterial = material;
+                mask.m_UnmaskMaterial = unmaterial;
+
+
+                //image.sprite = sprite;
+                //mask.m_MaskMaterial = material;
+                //mask.m_UnmaskMaterial = material;
 
                 return viewportRectTransform;
             }
 
             static Scrollbar CreateScrollbar(ScrollRect scrollRect, Scrollbar.Direction direction)
             {
-                // Create a Scrollbar GameObject and set its properties.
                 GameObject scrollbarObject = new GameObject("Scrollbar");
                 scrollbarObject.AddComponent<RectTransform>(); // Required for layout control.
 
-                // Set the Scrollbar's RectTransform properties.
                 RectTransform scrollbarRectTransform = scrollbarObject.GetComponent<RectTransform>();
                 scrollbarRectTransform.SetParent(scrollRect.transform, false);
 
-                // Add the Scrollbar component.
                 Scrollbar scrollbar = scrollbarObject.AddComponent<Scrollbar>();
                 scrollbar.direction = direction;
 
-                // Create a Scrollbar handle and set its properties.
                 GameObject handleObject = new GameObject("Handle");
                 handleObject.AddComponent<RectTransform>(); // Required for layout control.
 
-                // Set the Handle's RectTransform properties.
                 RectTransform handleRectTransform = handleObject.GetComponent<RectTransform>();
                 handleRectTransform.SetParent(scrollbarRectTransform, false);
 
@@ -242,10 +204,50 @@ namespace VSMenuModHelper
             [HarmonyPrefix]
             static void Initialize_Prefix2(OptionsController __instance)
             {
-                GameObject tabObject = __instance._TabContainer.gameObject;
+                //tabObject = __instance._TabContainer.gameObject;
+                //if (__instance._TabContainer.FindChild("ScrollView") != null)
+                //{
+                //    List<Transform> children = new();
+                //    for (int i = 0; i < _content.childCount; i++)
+                //    {
+                //        children.Add(_content.GetChild(i).transform);
+                //    }
+                //    children.ForEach((child) => child.SetParent(__instance._TabContainer, false));
+                //    GameObject.DestroyImmediate(__instance._TabContainer.FindChild("ScrollView").gameObject);
+                //}
+
+                //if (__instance._TabContainer.FindChild("ScrollView") == null)
+                //{
+                //    GameObject scrollViewObject = new GameObject("ScrollView");
+                //    scrollViewObject.transform.SetParent(tabObject.transform, false);
+
+                //    ScrollRect scrollRect = scrollViewObject.AddComponent<ScrollRect>();
+                //    //scrollRect.verticalScrollbar = CreateScrollbar(scrollRect, Scrollbar.Direction.BottomToTop);
+                //    scrollRect.horizontal = false;
+                //    scrollRect.scrollSensitivity = 100;
+                //    scrollRect.vertical = true;
+                //    scrollRect.viewport = CreateScrollViewViewport(scrollViewObject);
+                //    scrollRect.content = CreateScrollViewContent(scrollRect.viewport);
+
+                //    RectTransform rectTransform = scrollViewObject.GetComponent<RectTransform>();
+                //    rectTransform.anchorMin = Vector2.zero;
+                //    rectTransform.anchorMax = Vector2.one;
+                //    rectTransform.sizeDelta = Vector2.zero;
+                //    rectTransform.anchoredPosition = Vector2.zero;
+
+                //    _content = scrollRect.content;
+                //}
+            }
+
+            [HarmonyPatch(nameof(OptionsController.BuildPage))]
+            [HarmonyPostfix]
+            static void BuildPage_Postfix(OptionsController __instance, OptionsTabType type)
+            {
+                List<Transform> children = new();
+
+                tabObject = __instance._TabContainer.gameObject;
                 if (__instance._TabContainer.FindChild("ScrollView") != null)
                 {
-                    List<Transform> children = new();
                     for (int i = 0; i < _content.childCount; i++)
                     {
                         children.Add(_content.GetChild(i).transform);
@@ -259,32 +261,24 @@ namespace VSMenuModHelper
                     GameObject scrollViewObject = new GameObject("ScrollView");
                     scrollViewObject.transform.SetParent(tabObject.transform, false);
 
-                    // Add a ScrollRect component to the ScrollView GameObject.
                     ScrollRect scrollRect = scrollViewObject.AddComponent<ScrollRect>();
-                    scrollRect.content = CreateScrollViewContent(scrollRect);
+                    //scrollRect.verticalScrollbar = CreateScrollbar(scrollRect, Scrollbar.Direction.BottomToTop);
+                    scrollRect.horizontal = false;
+                    scrollRect.scrollSensitivity = 100;
+                    scrollRect.vertical = true;
+                    scrollRect.viewport = CreateScrollViewViewport(scrollViewObject);
+                    scrollRect.content = CreateScrollViewContent(scrollRect.viewport);
 
                     RectTransform rectTransform = scrollViewObject.GetComponent<RectTransform>();
                     rectTransform.anchorMin = Vector2.zero;
                     rectTransform.anchorMax = Vector2.one;
                     rectTransform.sizeDelta = Vector2.zero;
+                    rectTransform.anchoredPosition = Vector2.zero;
 
                     _content = scrollRect.content;
-
-                    // Create and set the viewport and scrollbar properties.
-                    scrollRect.viewport = CreateScrollViewViewport(scrollViewObject);
-                    scrollRect.verticalScrollbar = CreateScrollbar(scrollRect, Scrollbar.Direction.BottomToTop);
-                    scrollRect.horizontal = false;
-                    //scrollRect.horizontalNormalizedPosition = 0.3f;
-                    scrollRect.scrollSensitivity = 100;
-                    scrollRect.vertical = true;
                 }
-            }
 
-            [HarmonyPatch(nameof(OptionsController.BuildPage))]
-            [HarmonyPostfix]
-            static void BuildPage_Postfix(OptionsController __instance, OptionsTabType type)
-            {
-                List<Transform> children = new();
+                children = new();
                 for (int i = 0; i < __instance._TabContainer.childCount; i++)
                 {
                     if (__instance._TabContainer.GetChild(i).name != "ScrollView")
@@ -293,15 +287,11 @@ namespace VSMenuModHelper
                     }
                 }
                 children.ForEach((child) => child.SetParent(_content, false));
-                __instance._TabContainer.GetComponentInChildren<VerticalLayoutGroup>().SetDirty();
+
                 if (__instance._TabContainer.GetComponent<VerticalLayoutGroup>() != null)
                 {
                     GameObject.DestroyImmediate(__instance._TabContainer.GetComponent<VerticalLayoutGroup>());
-
                 }
-
-                Mask displayMask = GameObject.Find("UI/Canvas - App/Safe Area/View - Options/OptionsController/Display/Scroll/Viewport").GetComponent<Mask>();
-                displayMask.showMaskGraphic = false;
             }
 
             [HarmonyPatch(nameof(OptionsController.GenerateNavigation))]
